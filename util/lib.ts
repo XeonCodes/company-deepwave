70d069f6485046a7eeea0d3f425fb1939b9e393f
Oludowole Olumide <xeonncodes@gmail.com>
2024-09-03 17:15:10 +0100

    push

--- util/lib.ts
+++ util/lib.ts
@@ -25,6 +25,6 @@ export const sessionOptions: SessionOptions = {
   cookieOptions: {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    maxAge: 60 * 10,
+    maxAge: 60 * 60 * 24,
   },
 };
